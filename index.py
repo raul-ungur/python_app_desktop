88b9187d3b72e2c2169eee956c2fd5573ca73653
raul <raulungur547@gmail.com>
2026-07-06 14:41:13 +0300

    06/07/2026

--- index.py
+++ index.py
@@ -5,21 +5,25 @@ root.title("Task Manager")
 root.geometry("400x300")
 
 
-#input = StringVar()
-
 input_var = tk.StringVar()
 task_entry = tk.Entry(root,textvariable = input_var, font=('calibre',10,'normal'))
-task_entry.grid(row=0, column=0, padx=10, pady=10)
-def show_input():
-    input_get = input_var.get()
-    input_view = tk.Label(root, text= input_get, font=('calibre',10,'normal'))
-    input_view.grid(row=1, column=0, padx=10, pady=10) 
-    def remove_task():
-        input_view.config(text="")
-        button_remove.destroy()  
-    button_remove = tk.Button(root, text= "remove task", command= remove_task )
-    button_remove.grid(row=1, column=1, padx=10, pady=10)
+task_entry.pack()
+button = tk.Button(root, text= "add task", command= lambda: show_input())
+button.pack()
 
-button = tk.Button(root, text= "add task", command= show_input )
-button.grid(row=0, column=1, padx=10, pady=10)
+
+def show_input():
+    frame = tk.Frame(root, bg="blue")
+    frame.pack(fill=tk.X, padx=5, pady=5)
+    input_get = input_var.get()
+    input_view = tk.Label(frame, text= input_get, font=('calibre',10,'normal'))
+    input_view.pack(side=tk.LEFT, padx=5, pady=5) 
+    def remove_task():
+        frame.destroy()  
+    
+    button_remove = tk.Button(frame, text= "remove task", command= remove_task )
+    button_remove.pack(side=tk.RIGHT, padx=5, pady=5)
+
+
+
 root.mainloop()
